5e342382895d39c4f8617a71d846f03003fd4f5c
vvm <vvmspace@gmail.com>
2021-01-08 22:49:32 +0500

    feature(core): .env support

--- src/tinkoff/tinkoff.module.ts
+++ src/tinkoff/tinkoff.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TinkoffService } from './tinkoff.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [TinkoffService],
+  imports: [ConfigModule.forRoot({
+    validationOptions: {
+      allowUnknown: false
+    }
+  })],
 })
 export class TinkoffModule {}
